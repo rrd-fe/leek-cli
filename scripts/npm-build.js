f3e422c6bde33e029c3740e8c8168a37c335f481
dushaobin <dushaobin@we.com>
2018-09-20 11:26:56 +0800

    提交新版本

--- scripts/npm-build.js
+++ scripts/npm-build.js
@@ -4,34 +4,34 @@ var shelljs = require('shelljs');
 
 var buildedDir = './.builded';
 
-console.log('创建安装路径：'+buildedDir);
+console.log('创建安装路径：' + buildedDir);
 
-//删除已存在的文件
-shelljs.rm('-rf',buildedDir);
+// 删除已存在的文件
+shelljs.rm('-rf', buildedDir);
 
-//创建新目录
+// 创建新目录
 shelljs.mkdir(buildedDir);
 
-console.log("编译文件：开始");
+console.log('编译文件：开始');
 
-//编译bin文件
+// 编译bin文件
 shelljs.exec('./node_modules/babel-cli/bin/babel.js ./bin -d .builded/bin');
 
-//编译源代码
+// 编译源代码
 shelljs.exec('./node_modules/babel-cli/bin/babel.js ./src -d .builded/src');
 
-//编译cli.js文件
+// 编译cli.js文件
 shelljs.exec('./node_modules/babel-cli/bin/babel.js ./cli.js --out-file  .builded/cli.js');
 
 // 复制command 命令
 console.log('复制command 文件');
-shelljs.cp('-Rf','./src/shell','.builded/src/shell');
+shelljs.cp('-Rf', './src/shell', '.builded/src/shell');
 
 console.log('复制README.md文件');
-shelljs.cp('./README.md','.builded/README.md');
+shelljs.cp('./README.md', '.builded/README.md');
 
-//复制readme文件
+// 复制readme文件
 console.log('复制package.json文件');
-shelljs.cp('./package.json','.builded/package.json');
+shelljs.cp('./package.json', '.builded/package.json');
 
 console.log('编译文件：结束');
--- scripts/npm-publish.js
+++ scripts/npm-publish.js
@@ -5,7 +5,8 @@ var shelljs = require('shelljs');
 shelljs.exec('npm run build');
 
 console.log('正在发布项目...');
-//在全局安装
+
+// 在全局安装
 shelljs.exec('npm publish .builded/');
 
 console.log('成功发布到npm');
